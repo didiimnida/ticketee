7f1ce2c6e49651fccb7f1dc653defaa549e369f7
Diana Hilton <diana.hilton@gmail.com>
2015-03-17 16:38:57 -0700

    Lock create, edit and delete on projects controller and project views for admins only.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
 			end
 		end	
 	end
+
+	def admins_only(&block)
+		block.call if current_user.try(:admin?)
+	end
+	#Write a helper method to take a block from the view.  
 end
