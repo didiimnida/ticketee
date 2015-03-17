0bc376382ee6bf63fa64c8f87cf5b45633fca4f8
Diana Hilton <diana.hilton@gmail.com>
2015-03-17 13:44:59 -0700

    Associated users with tickets when creating tickets. AuthenticationHelpers not working yet.

--- spec/features/deleting_tickets_spec.rb
+++ spec/features/deleting_tickets_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 feature "Deleting tickets" do
 	let!(:project) {FactoryGirl.create(:project)}
-	let!(:ticket) {FactoryGirl.create(:ticket, project: project)}
+	let!(:user) {FactoryGirl.create(:user)}
+  	let!(:ticket) { 
+    	FactoryGirl.create(:ticket, project: project, user: user)}
 
 	before do 
+		visit '/signin'
+    	fill_in "Name", with: user.name
+    	fill_in "Password", with: user.password
+    	click_button 'Sign in'
+    	expect(page).to have_content("Signed in successfully.")
+
 		visit "/"
 		click_link project.name
 		click_link ticket.title
--- spec/features/editing_tickets_spec.rb
+++ spec/features/editing_tickets_spec.rb
@@ -2,9 +2,20 @@ require 'rails_helper'
 
 feature "Editing tickets" do
   let!(:project) { FactoryGirl.create(:project) }
-  let!(:ticket) { FactoryGirl.create(:ticket, project: project) }
+  let!(:user) {FactoryGirl.create(:user)}
+  let!(:ticket) do 
+    ticket = FactoryGirl.create(:ticket, project: project)
+    ticket.update(user: user)
+    ticket 
+    end
   
   before do
+    visit '/signin'
+    fill_in "Name", with: user.name
+    fill_in "Password", with: user.password
+    click_button 'Sign in'
+    expect(page).to have_content("Signed in successfully.")
+    
     visit '/'
     click_link project.name
     click_link ticket.title
