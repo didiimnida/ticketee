da08a0e3504143c29d4d4624817964d746b0a6ca
Diana Hilton <diana.hilton@gmail.com>
2015-03-17 18:52:50 -0700

    Can create admin users and regular users through the admin backend.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -18,13 +18,15 @@ class Admin::UsersController < Admin::BaseController
   	else
     	flash.now[:alert] = "User has not been created."
     	render :action => "new"
-	end 
+	  end 
   end
 
   private
   	def user_params
     	params.require(:user).permit(:name,
                                  :password,
-                                 :password_confirmation)
+                                 :password_confirmation, 
+                                 :email,
+                                 :admin)
   	end
 end
